f692909289d132fcda52a5d4f79ecaba46037929
mikiyas mebrate <mikiyasmebrate@gmail.com>
2023-10-09 15:07:03 +0300

    account model added and minor changes

--- UserManagement/models.py
+++ UserManagement/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-# Create your models here.
 class CustomUser(AbstractUser):
     is_user = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
