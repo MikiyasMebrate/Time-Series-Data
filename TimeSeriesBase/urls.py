ec0d36fe70277e0dcb90fc40654b1e398128a182
Selam Girmay <121090365+selena235@users.noreply.github.com>
2023-10-09 15:53:09 +0300

    user template added

--- TimeSeriesBase/urls.py
+++ TimeSeriesBase/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from .views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('TimeSeriesBase/', include('TimeSeriesBase.urls')),
+    path('index/',index,name="index"),
+    path('about/',about,name="about"),
+    path('contact/',contact,name="contact"),
+    path('profile/',profile,name="profile"),
+    
 ]
