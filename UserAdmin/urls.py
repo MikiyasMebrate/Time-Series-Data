80a1bb78aa8dbe860ea8f0f17ce64a4bafb0e36b
HabtamuTesfayeACT <126651482+HabtamuTesfayeACT@users.noreply.github.com>
2023-10-10 15:56:19 +0300

    forms next

--- UserAdmin/urls.py
+++ UserAdmin/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path('measurement/', views.measurement, name="user-admin-measurement"),
     path('profile/', views.profile, name="user-admin-profile"),
     path('source/', views.source, name="user-admin-source"),
+        path('location/<int:pk>', views.location_detail, name="user-location-detail"),
+    path('location-delete/<int:pk>', views.delete_location, name='user-location-delete'),
     path('topic/', views.topic, name="user-admin-topic"),
     path('users/', views.users_list, name="user-admin-user-list")
 ]
