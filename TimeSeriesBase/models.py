fef14b31f06d64622dfa1a450c0a8f119da49f9f
mikiyas mebrate <mikiyasmebrate@gmail.com>
2023-10-16 09:13:31 +0300

    admin side bar and dataPoint model updated

--- TimeSeriesBase/models.py
+++ TimeSeriesBase/models.py
@@ -73,8 +73,8 @@ class Indicator_Point(models.Model):
 
 
 class DataPoint(models.Model):
-    year_EC = models.CharField(max_length=50)
-    year_GC = models.CharField(max_length=50)
+    year_EC = models.CharField(max_length=50, null=True, blank=True)
+    year_GC = models.CharField(max_length=50, null=True, blank=True)
     year_start_EC = models.CharField(max_length=50, null=True, blank=True)
     year_start_GC = models.CharField(max_length=50, null=True, blank=True)
     year_end_EC = models.CharField(max_length=50, null=True, blank=True)
