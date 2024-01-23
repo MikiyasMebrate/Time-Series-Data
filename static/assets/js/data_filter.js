e608e51ad281c1db7dcb7fea58615269864945e9
Habtamu Tesfaye <habtamutesfaye.com@gmail.com>
2024-01-23 09:16:09 -0800

    fix year reverse problem

--- static/assets/js/data_filter.js
+++ static/assets/js/data_filter.js
@@ -3337,19 +3337,36 @@ $(document).ready(function () {
 
   });
 
-  // // Add event listener to map button to be active color
-  // $("#mapbutton").click(function () {
-  //   // Show chart
-  //   $(".data-display #map").show();
 
-  //   // Hide table
-  //   $(".data-display #chart").hide();
-  //   $(".data-display #table-container").hide();
+});
 
-  //   // Set chart button active
-  //   $("#mapbutton").addClass("active");
-  //   $("#chartButton").removeClass("active");
-  //   $("#tableButton").removeClass("active");
-  // });
+  
+const snippets = document.querySelectorAll('.snippet');
 
+for (let i = 0; i < snippets.length; i++) {
+  snippets[i].addEventListener('mouseleave', clearTooltip);
+  snippets[i].addEventListener('blur', clearTooltip);
+}
+
+function showTooltip(el, msg) {
+  el.setAttribute('class', 'snippet tooltip');
+  el.setAttribute('aria-label', msg);
+}
+
+function clearTooltip(e) {
+  e.currentTarget.setAttribute('class', 'snippet');
+  e.currentTarget.removeAttribute('aria-label');
+}
+
+const clipboardSnippets = new ClipboardJS('.snippet', {
+  text: trigger => trigger.getAttribute('data-title')
+});
+
+clipboardSnippets.on('success', e => {
+  e.clearSelection();
+  showTooltip(e.trigger, 'Copied!');
+});
+
+clipboardSnippets.on('error', e => {
+  showTooltip(e.trigger, 'Copy failed!');
 });
