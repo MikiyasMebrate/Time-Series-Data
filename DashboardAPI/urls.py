43b8dcaa76ce5c64eb0f44d74a45b86ef8af2706
MikiyasMebrate <mikiyasmebrate@gmail.com>
2024-05-25 09:58:31 +0300

    time.sleep removed and indidcator detail resolved

--- DashboardAPI/urls.py
+++ DashboardAPI/urls.py
@@ -1,4 +1,5 @@
-from .views import (topic_lists, index , category_list , category_detail_lists , pie_chart_data)
+
+from .views import (topic_lists, index , category_list , category_detail_lists, indicator_detail,pie_chart_data)
 from django.urls import path
 
 urlpatterns = [
@@ -6,5 +7,6 @@ urlpatterns = [
     path('topic_lists/',topic_lists, name="topic_lists"),
     path('pie_chart_data/',pie_chart_data, name="pie_chart_data"),
     path('category_list/<int:id>/',category_list, name="category_list"),
-    path('category_detail_list/<int:id>/',category_detail_lists, name="category_detail_lists")
+    path('category_detail_list/<int:id>/',category_detail_lists, name="category_detail_lists"),
+    path('indicator-detail/<int:id>/',indicator_detail, name='indicator-detail'),
 ]
